527fe0cf3e3ca47c49a8b5a9bf002c46e16422af
votruonglong <votruonglong.work@gmail.com>
2023-11-12 13:21:33 +0700

    Them Xoa Sua Category

---
+++ Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,85 @@
+﻿using ProjectShoeShop.DAL;
+using ProjectShoeShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectShoeShop.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Admin/Category
+        ShoeShopDbContext db = new ShoeShopDbContext();
+        public ActionResult Index()
+        {
+            var list = db.Categories.ToList();
+            return View(list);
+        }
+
+        public ActionResult Create()
+        {
+
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Category obj)
+        {
+            db.Categories.Add(obj);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(string id)
+        {
+            var category = db.Categories.FirstOrDefault(x => x.Id == id);
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Category obj)
+        {
+            var existingCategory = db.Categories.FirstOrDefault(x => x.Id == obj.Id);
+
+            if (existingCategory != null)
+            {
+                existingCategory.Name = obj.Name;
+                existingCategory.Description = obj.Description;
+
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+
+        }
+
+        public ActionResult Delete(string id)
+        {
+            var Category = db.Categories.FirstOrDefault(x => x.Id == id);
+
+            return View(Category);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirm(string id)
+        {
+            var existingCategory = db.Categories.FirstOrDefault(x => x.Id == id);
+
+            if (existingCategory != null)
+            {
+                db.Categories.Remove(existingCategory);
+
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+
+        }
+    }
+}
---
+++ Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectShoeShop.Areas.Admin.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Admin/Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
