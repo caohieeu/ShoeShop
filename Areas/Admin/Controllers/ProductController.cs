fa08a0f87a625389aaaa95bfa185ed1d693cd0cd
votruonglong <votruonglong.work@gmail.com>
2023-11-13 14:30:57 +0700

    Them Xoa Sua Product, chưa có thêm file image

--- Areas/Admin/Controllers/ProductController.cs
+++ Areas/Admin/Controllers/ProductController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ProjectShoeShop.DAL;
+using ProjectShoeShop.Models;
+using ProjectShoeShop.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +11,119 @@ namespace ProjectShoeShop.Areas.Admin.Controllers
 {
     public class ProductController : Controller
     {
+        ShoeShopDbContext db = new ShoeShopDbContext();
+
         // GET: Admin/Product
+
         public ActionResult Index()
         {
-            return View();
+            var list = db.Products.ToList();
+            return View(list);
+        }
+
+        public ActionResult Create()
+        {
+            var category = db.Categories.ToList(); 
+            var brand = db.Brands.ToList();
+            ProductVM productVM = new ProductVM()
+            {
+                CategoryList = category.Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString(),
+                }),
+                BrandList = brand.Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString(),
+                }),
+                Product = new Product(),
+            };
+
+            return View(productVM);
+        }
+
+        [HttpPost]
+        public ActionResult Create(ProductVM obj)
+        {
+            
+                if (ModelState.IsValid)
+                {
+                    db.Products.Add(obj.Product);
+                    db.SaveChanges();
+                }
+            
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(string id)
+        {
+            
+            var category = db.Categories.ToList();
+            var brand = db.Brands.ToList();
+            ProductVM productVM = new ProductVM()
+            {
+                CategoryList = category.Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString(),
+                }),
+                BrandList = brand.Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString(),
+                }),
+                Product = db.Products.FirstOrDefault(u => u.Id == id)
+        };
+            return View(productVM);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(ProductVM obj)
+        {
+
+            if (ModelState.IsValid)
+            {
+                var existingProduct = db.Products.FirstOrDefault(x => x.Id == obj.Product.Id);
+
+                if (existingProduct != null)
+                {
+                    existingProduct.Name = obj.Product.Name;
+                    existingProduct.Description = obj.Product.Description;
+                    existingProduct.ImageURL = obj.Product.ImageURL;
+                    existingProduct.Price = obj.Product.Price;
+                    existingProduct.Stock = obj.Product.Stock;
+                    existingProduct.BrandID = obj.Product.BrandID;
+                    existingProduct.CategoryID = obj.Product.CategoryID;
+
+                    db.SaveChanges();
+                }
+
+                
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(string id)
+        {
+            var product = db.Products.FirstOrDefault(u => u.Id == id);
+            return View(product);
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirm(string id)
+        {
+            var existingProduct = db.Products.FirstOrDefault(x => x.Id == id);
+
+            if (existingProduct != null)
+            {
+                db.Products.Remove(existingProduct);
+
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+
         }
     }
 }
---
+++ ViewModel/ProductVM.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using ProjectShoeShop.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectShoeShop.ViewModel
+{
+    public class ProductVM
+    {
+        public Product Product { get; set; }
+        [ValidateNever]
+        public IEnumerable<SelectListItem> CategoryList { get; set; }
+        [ValidateNever]
+        public IEnumerable<SelectListItem> BrandList { get; set; }
+
+    }
+}
