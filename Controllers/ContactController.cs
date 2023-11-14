dec64c9c5172354171af04d1f3fd7a489d059822
tandzp0o <tannguyen0916@gmail.com>
2023-11-14 22:16:46 +0700

    add view(contact, about)

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectShoeShop.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult ContactUs()
+        {
+            return View();
+        }
+    }
+}
